b8f308566fa77015b8e376cd7d2b2f06c2ee9baa
Ben Gamari <ben@smart-cactus.org>
2017-01-18 00:24:28 -0500

    update to 2b61f52a0d0d636fb468756728c3ee0f5def8304

--- ghc_config.py
+++ ghc_config.py
@@ -1,9 +1,10 @@
 extlinks = {
     'base-ref': ('../libraries/base-4.10.0.0/%s', ''),
     'cabal-ref': ('../libraries/Cabal-1.25.0.0/%s', ''),
+    'compact-ref': ('../libraries/compact-1.0.0.0/%s', ''),
     'ghc-prim-ref': ('../libraries/ghc-prim-0.5.0.0/%s', ''),
     'ghc-ticket': ('http://ghc.haskell.org/trac/ghc/ticket/%s', 'Trac #'),
     'ghc-wiki': ('http://ghc.haskell.org/trac/ghc/wiki/%s', 'Trac #'),
 }
 
-version = '8.1.20170116'
+version = '8.1.20170117'
